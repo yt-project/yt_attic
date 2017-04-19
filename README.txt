685d0a44101d3888ad17dccdf16c01910b9cf601
Britton Smith <brittonsmith@gmail.com>
2017-04-19 16:17:56 -0700

    Adding setup.py and finishing README.

--- README.txt
+++ README.txt
@@ -2,3 +2,19 @@ This is the yt attic, a repository of yt analysis modules that have
 fallen by the wayside and could use a new home.  If you find anything
 interesting in here, please feel free to take it over.
 
+These modules can be installed as a python package by doing:
+
+python setup.py develop
+
+Once installed, they can be imported as yt.extensions.attic.  For
+example,
+
+from yt.extensions.attic.star_analysis.api import StarFormationRate
+
+These modules come with all documentation that was written for them.
+To see these docs, do the following:
+
+cd doc
+make html
+
+Enjoy!
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "yt_attic",
+    version = "0.0.1",
+    author = "The yt project",
+    author_email = "yt-dev@lists.spacepope.org",
+    description = ("The yt attic."),
+    long_description = ("""Disused yt analysis modules."""),
+    license = "BSD",
+    keywords = ["simulation", "spectra", "astronomy", "astrophysics"],
+    url = "http://yt-project.org/",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    install_requires=['yt', 'h5py', 'numpy', 'matplotlib', 'astropy', 'requests'],
+)
